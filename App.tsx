abbd99f492017489966073ebc30b86f868d81a0e
Sebastian Cyrych <scyrych@uw.co.uk>
2025-12-09 14:40:08 +0100

    working turbomodule example

--- App.tsx
+++ App.tsx
@@ -1,27 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import NativeLocalStorage from './specs/NativeLocalStorage';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const EMPTY = '<empty>';
+
+function App(): React.JSX.Element {
+  const [value, setValue] = React.useState<string | null>(null);
+
+  const [editingValue, setEditingValue] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    const storedValue = NativeLocalStorage?.getItem('myKey');
+    setValue(storedValue ?? '');
+  }, []);
+
+  function saveValue() {
+    NativeLocalStorage?.setItem(editingValue ?? EMPTY, 'myKey');
+    setValue(editingValue);
+  }
+
+  function clearAll() {
+    NativeLocalStorage?.clear();
+    setValue('');
+  }
+
+  function deleteValue() {
+    NativeLocalStorage?.removeItem('myKey');
+    setValue('');
+  }
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text style={styles.text}>
+        Current stored value is: {value ?? 'No Value'}
+      </Text>
+      <TextInput
+        placeholder="Enter the text you want to store"
+        style={styles.textInput}
+        onChangeText={setEditingValue}
+      />
+      <Button title="Save" onPress={saveValue} />
+      <Button title="Delete" onPress={deleteValue} />
+      <Button title="Clear" onPress={clearAll} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  text: {
+    margin: 10,
+    fontSize: 20,
+  },
+  textInput: {
+    margin: 10,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 5,
   },
 });
 
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by Sebastian Cyrych on 09/12/2025.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
